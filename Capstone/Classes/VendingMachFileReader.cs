e47b74cb48d628322edfb4e4d34f08d45907bc76
Ilona Seaman <i.seaman@outlook.com>
2017-05-25 16:47:43 -0400

    updates from ilona's computer

---
+++ Capstone/Classes/VendingMachFileReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Classes
+{
+    public static class VendingMachFileReader
+    {
+        public static void ReadFile()
+        {
+            string directory = Environment.CurrentDirectory;
+            string filename = "vendingmachine.csv";
+
+            string fullPath = Path.Combine(directory, filename);
+
+            Dictionary<string, List<Item>> dictionary = new Dictionary<string, List<Item>>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(fullPath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] items = line.Split('|');
+
+                        List<VMItems> items = new List<VMItems>();
+                        for (int i = 0; i < 5; i++)
+                        {
+                            if (slot starts with A)
+                            {
+                        }
+                    }
+                }
+                dictionary.Add(slot, items);
+            }
+            }
+    }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error reading the file");
+                Console.WriteLine(e.Message);
+            }
+}
+    }
+}
---
+++ Capstone/Classes/VendingMachFileWriter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Classes
+{
+    public static class VendingMachFileWriter
+    {
+        public static void VMFileWriter(string path)
+        {
+            string directory = Environment.CurrentDirectory;
+            string filename = "vendingMachine_updated.csv";
+            string fullPath = Path.Combine(directory, filename);
+        }
+        public static void LogMessage(string message)
+        {
+            string directory = Environment.CurrentDirectory;
+            string filename = "Log.txt";
+            string fullPath = Path.Combine(directory, filename);
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filename))
+                {
+                    sw.WriteLine(DateTime.UtcNow + method.GetType().Name + "transactionAmount" + VendingMachine.Balance);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Error occured writing file " + ex.ToString());
+            }
+
+        }
+    }
+}
+
